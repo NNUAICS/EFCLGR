64235747a80ba1d8efedf9b49a361ea174888e32
wjkweb <wjkweb@163.com>
2026-02-11 10:34:39 +0800

    Update L2_distance.m

--- L2_distance.m
+++ L2_distance.m
@@ -20,33 +20,6 @@ function d = L2_distance(a, b)
 %    A = rand(400,100); B = rand(400,200);
 %    d = distance(A,B);
 
-% Author   : Roland Bunschoten
-%            University of Amsterdam
-%            Intelligent Autonomous Systems (IAS) group
-%            Kruislaan 403  1098 SJ Amsterdam
-%            tel.(+31)20-5257524
-%            bunschot@wins.uva.nl
-% Last Rev : Wed Oct 20 08:58:08 MET DST 1999
-% Tested   : PC Matlab v5.2 and Solaris Matlab v5.3
-
-% Copyright notice: You are free to modify, extend and distribute 
-%    this code granted that the author of the original code is 
-%    mentioned as the original author of the code.
-
-% Fixed by JBT (3/18/00) to work for 1-dimensional vectors
-% and to warn for imaginary numbers.  Also ensures that 
-% output is all real, and allows the option of forcing diagonals to
-% be zero.  
-%
-%
-
-% This file is part of the Matlab Toolbox for Dimensionality Reduction.
-% The toolbox can be obtained from http://homepage.tudelft.nl/19j49
-% You are free to use, change, or redistribute this code in any way you
-% want for non-commercial purposes. However, it is appreciated if you 
-% maintain the name of the original author.
-%
-% (C) Laurens van der Maaten, Delft University of Technology
 
 
     if nargin < 2
